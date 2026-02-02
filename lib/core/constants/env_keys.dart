a5713616b171de71195a3c0c3a52552b16640976
sudipeb <sudip.koirala@ebpearls.com>
2026-02-02 09:56:43 +0545

    :star:environment keys setup

---
+++ lib/core/constants/env_keys.dart
@@ -0,0 +1,16 @@
+class EnvKeys {
+  ///[Environment Variables]
+  static const String devEnv = 'DevelopmentEnv';
+  static const String stageEnv = 'StageEnv';
+  static const String prodEnv = 'ProdEnv';
+
+  ///[Environment Variable path]
+  static const String devEnvPath = '.env_dev';
+  static const String stageEnvPath = '.env_stage';
+  static const String prodEnvPath = '.env_prod';
+
+  ///[Environment's constants fields]
+  static const String baseUrlKey = 'BASE_URL';
+  static const String graphEndPointKey = 'GRAPHQL';
+  static const String apiClientId = 'ANDROID_CLIENT_ID';
+}
